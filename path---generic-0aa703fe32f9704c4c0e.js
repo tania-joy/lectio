2201ffdabe8ca3962eef255dc95a561d61bbf65b
taniajoy <tania.joy@citrusinformatics.com>
2018-05-15 15:11:46 +0530

    Updates

---
+++ path---generic-0aa703fe32f9704c4c0e.js
@@ -0,0 +1,2 @@
+webpackJsonp([52547762336625],{822:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Lectio Generator"}}},pathContext:{}}}});
+//# sourceMappingURL=path---generic-0aa703fe32f9704c4c0e.js.map
---
+++ path---index-0aa703fe32f9704c4c0e.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{861:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Lectio Generator"}}},pathContext:{}}}});
+//# sourceMappingURL=path---index-0aa703fe32f9704c4c0e.js.map
